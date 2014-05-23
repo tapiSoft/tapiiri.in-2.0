38d23abdbf4c3746366a3d9ee8472fba7bae13b2
Zmo <simo.kolppo@gmail.com>
2014-05-24 02:33:23 +0300

    Generated the scaffolds with yeoman and added some controllers and many other things

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var tapiiriinApp = angular.module('tapiiriinApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'tapiiriControllers'
+]);
+
+tapiiriinApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/e-sports', {
+      templateUrl: 'views/e-sports.html',
+      controller: 'E-sportsCtrl'
+    })
+    .when('/powerlifting', {
+      templateUrl: 'views/powerlifting.html',
+      controller: 'PowerliftingCtrl'
+    })
+    .when('/software', {
+      templateUrl: 'views/software.html',
+      controller: 'SoftwareCtrl'
+    })
+    .when('/broadcasting', {
+      templateUrl: 'views/broadcasting.html',
+      controller: 'BroadcastingCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var tapiiriControllers = angular.module('tapiiriControllers', []);
+
+tapiiriControllers.controller('MainCtrl', function ($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+});
+
+tapiiriControllers.controller('HeaderCtrl', function ($scope, $location) {
+  $scope.isActive = function (viewLocation) {
+    return viewLocation === $location.path();
+  };
+});
+
+tapiiriControllers.controller('E-sportsCtrl', function ($scope) {
+
+});
+
+tapiiriControllers.controller('PowerliftingCtrl', function ($scope) {
+
+});
+
+tapiiriControllers.controller('SoftwareCtrl', function ($scope) {
+
+});
+
+tapiiriControllers.controller('BroadcastingCtrl', function ($scope) {
+
+});
